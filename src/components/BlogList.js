1379dd911198e1f38a13c636290ca842d10dae02
nuri park <nuripark@172.20.nate.com>
2023-02-22 00:12:04 +0900

    Pagination4-Blogs페이지정렬

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -15,14 +15,20 @@ const BlogList = ({ isAdmin }) => {
   const [loading, setLoading] = useState(true);
 
   const getPosts = (page = 1) => {
+    let params = {
+      _page: page,
+      _limit: 5,
+      _sort: "id",
+      _order: "desc",
+    };
+
+    if (!isAdmin) {
+      params = { ...params, publish: true };
+    }
+
     axios
       .get(`http://localhost:3001/posts`, {
-        params: {
-          _page: page,
-          _limit: 5,
-          _sort: "id",
-          _order: "desc",
-        },
+        params,
       })
       .then((res) => {
         setPosts(res.data);
@@ -50,30 +56,26 @@ const BlogList = ({ isAdmin }) => {
   }
 
   const renderBlogList = () => {
-    return posts
-      .filter((post) => {
-        return isAdmin || post.publish;
-      })
-      .map((post) => {
-        return (
-          <Card
-            key={post.id}
-            title={post.title}
-            onClick={() => navigate(`/blogs/${post.id}`)}
-          >
-            {isAdmin ? (
-              <div>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={(e) => deleteBlog(e, post.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            ) : null}
-          </Card>
-        );
-      });
+    return posts.map((post) => {
+      return (
+        <Card
+          key={post.id}
+          title={post.title}
+          onClick={() => navigate(`/blogs/${post.id}`)}
+        >
+          {isAdmin ? (
+            <div>
+              <button
+                className="btn btn-danger btn-sm"
+                onClick={(e) => deleteBlog(e, post.id)}
+              >
+                Delete
+              </button>
+            </div>
+          ) : null}
+        </Card>
+      );
+    });
   };
 
   return (
