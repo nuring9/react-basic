de4baae1d332f2ef849116416494b4616e505a91
nuri park <nuripark@MacBookPro.local>
2023-02-27 16:40:07 +0900

    새로고침 시 로그인 정보 풀리지 않도록 Local Storage 설정

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import routes from "./routes";
 import NaveBar from "./components/NaveBar";
 import Toast from "./components/Toast";
 import useToast from "./hooks/toast";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import ProtectedRoute from "./ProtectedRoute";
+import { useEffect, useState } from "react";
+import { login } from "./store/authSlice";
+
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const toasts = useSelector((state) => state.toast.toasts);
+
+  const [loading, setLoading] = useState(true);
+
   const { deleteToast } = useToast();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn")) {
+      dispatch(login());
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <Router>
       <NaveBar />
