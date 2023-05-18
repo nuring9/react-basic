8f60112a9a6906f5c4e7a6de58d7b21fc775779d
Nuri <for9595@google.com>
2023-05-18 13:25:39 +0900

    배경

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <h1>프로젝트로 배우는 리액트</h1>;
+  return (
+    <>
+      <div class="p-3 mb-2 bg-secondary text-white">.bg-secondary</div>
+      <h1>프로젝트로 배우는 리액트</h1>
+    </>
+  );
 };
 
 export default HomePage;
